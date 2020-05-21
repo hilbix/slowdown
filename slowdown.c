f886b9cdd9750bd41992642ac30fdcbacc54b7e6
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-05-22 00:51:19 +0200

    works now

--- slowdown.c
+++ slowdown.c
@@ -24,6 +24,7 @@
 #include "slowdown_version.h"
 
 static const char	*arg0;
+static int		debug;
 static int		verbose;
 #if 0
 static int		jail, quiet, recursive;
@@ -120,10 +121,26 @@ sig_usr2(void)
 }
 #endif
 
-static int			is_delay;
-static struct timespec		delay;
-static unsigned long long	delay_us;
-static long			sleeps;
+struct delay_info
+  {
+    int			is_delay;
+    long long		sleeps;
+    struct timespec	delay;
+    unsigned long long	delay_us;
+    long long		miss_us;
+  };
+
+struct trace_info
+  {
+    pid_t	pid;
+    int		sta, sig, evt;
+    int		detach, firststop;
+    int		retval, retval_set;
+    int		options;
+    siginfo_t	si;
+    struct delay_info	*delay;
+    long long		loops;
+  };
 
 /* This obviously isn't precise.
  *
@@ -135,26 +152,26 @@ static long			sleeps;
  * implement this today.
  */
 static int
-do_delay(void)
+do_delay(struct delay_info *delay)
 {
 #if 0
   struct timespec	elapsed;
 #endif
   int			ret;
   struct timeval	a, b;
-  static long long	miss_us;
 
-  if (!is_delay)
+  if (!delay->is_delay)
     return 0;
-  if ((miss_us+=delay_us)<0)
+  if ((delay->miss_us += delay->delay_us)<0)
     return 0;
-  sleeps++;
+  delay->sleeps++;
+
   if (gettimeofday(&a, NULL))
     OOPS("gettimeofday");
-  ret	= nanosleep(&delay, NULL);
+  ret	= nanosleep(&delay->delay, NULL);
   if (gettimeofday(&b, NULL))
     OOPS("gettimeofday");
-  miss_us	-= (b.tv_sec-a.tv_sec)*1000000ull+b.tv_usec-a.tv_usec;
+  delay->miss_us	-= (b.tv_sec-a.tv_sec)*1000000ull+b.tv_usec-a.tv_usec;
 #if 0
   if (!ret)
     {
@@ -173,11 +190,11 @@ do_delay(void)
  * Note: According to manual, ptrace() never delivers EINTR
  */
 static void
-ptrace__attach(pid_t pid)
+ptrace__attach(struct trace_info *inf)
 {
-  if (ptrace(PTRACE_ATTACH, pid, NULL, NULL))
-    OOPS("cannot attach to %ld", (long)pid);
-  status(pid, "attached");
+  if (ptrace(PTRACE_ATTACH, inf->pid, NULL, NULL))
+    OOPS("cannot attach to %ld", (long)inf->pid);
+  status(inf->pid, "attached");
 }
 
 /* Like ptrace__attach, but for children
@@ -193,14 +210,21 @@ ptrace__traceme(void)
  * a syscall().  (This only happens if ptrace() is active.)
  */
 static void
-ptrace__syscall(pid_t pid, int sig)
+ptrace__syscall(struct trace_info *inf)
 {
-  if (ptrace(PTRACE_SYSCALL, pid, NULL, sig))
+  if (ptrace(PTRACE_SYSCALL, inf->pid, NULL, inf->sig))
     OOPS("ptrace(PTRACE_SYSCALL)");
 }
 
-#define	PTRACE__WSTOPSIG(sta)	(WSTOPSIG(sta) & ~0x80)
-#define	PTRACE__WIFPTRACED(sta)	(WSTOPSIG(sta) & 0x80)		/* WIFPTRACED() is missing	*/
+/* We need PTRACE_LISTEN in case the child is in groupstop
+ * to avoid it being started when it is not supposed to.
+ */
+static void
+ptrace__listen(struct trace_info *inf)
+{
+  if (ptrace(PTRACE_LISTEN, inf->pid, NULL, inf->sig))
+    OOPS("ptrace(PTRACE_LISTEN)");
+}
 
 /* ptrace() delivers SIGTRAP each time the process is stopped.
  *
@@ -209,11 +233,11 @@ ptrace__syscall(pid_t pid, int sig)
  * will be delivered as PTRACE__WSTOPSIG
  */
 static void
-ptrace__setoptions(pid_t pid)
+ptrace__setoptions(struct trace_info *inf)
 {
-  if (ptrace(PTRACE_SETOPTIONS, pid, NULL, PTRACE_O_TRACESYSGOOD))
+  if (ptrace(PTRACE_SETOPTIONS, inf->pid, NULL, inf->options))
     OOPS("ptrace(PTRACE_SETOPTIONS)");
-  status(pid, "options");
+  status(inf->pid, "options");
 }
 
 /* Detach from a child.
@@ -221,85 +245,131 @@ ptrace__setoptions(pid_t pid)
  * AFAICS this must be done in the right situation
  */
 static void
-ptrace__detach(pid_t pid)
+ptrace__detach(struct trace_info *inf)
 {
-  if (ptrace(PTRACE_DETACH, pid, NULL, NULL))
+  if (ptrace(PTRACE_DETACH, inf->pid, NULL, NULL))
     OOPS("ptrace(PTRACE_DETACH)");
-  status(pid, "detached");
+  status(inf->pid, "detached");
 }
 
-/* This routine is too long
- */
 static int
-delay_trace(char **argv)
+ptrace__getsiginfo(struct trace_info *inf)
 {
-  pid_t	pid;
-  int	sta;
+  return ptrace(PTRACE_GETSIGINFO, inf->pid, 0, &inf->si);
+}
+
+enum ptrace__stops
+  {
+  PTRACE__SIGNAL,
+  PTRACE__OPTIONS_MISSING,
+  PTRACE__SYSCALL,
+  PTRACE__GROUPSTOP,
+  PTRACE__EVENTSTOP,
+  PTRACE__TRAP,		/* I can observe spurious TRAPs, sigh	*/
+  };
+
+static enum ptrace__stops
+ptrace__event(struct trace_info *inf)
+{
+  inf->sig	= inf->sta>>8;
+  if (inf->sig & 0x80)
+    {
+      inf->sig	= 0;
+      return PTRACE__SYSCALL;	/* options already applied	*/
+    }
+
+  if (inf->sig & ~0xff)
+    switch (inf->evt = inf->sig >> 8)
+      {
+      default:
+        warn("unknown extended status 0x%x", inf->sta);
+        /* intended fall though	*/
+      case PTRACE_EVENT_VFORK:
+      case PTRACE_EVENT_FORK:
+      case PTRACE_EVENT_CLONE:
+      case PTRACE_EVENT_VFORK_DONE:
+      case PTRACE_EVENT_EXEC:
+      case PTRACE_EVENT_EXIT:
+      case PTRACE_EVENT_SECCOMP:
+        inf->sig	&= 0xff;
+        return PTRACE__EVENTSTOP;
+
+      /* PTRACE_EVENT_STOP is the only event stop which can be group stop either.
+       * Isn't this name a little bit ironic, perhaps?
+       */
+      case PTRACE_EVENT_STOP:
+        /* According to documentation, which sucks a lot BTW,
+         * PTRACE_EVENT_STOP actually is a group stop condition.
+         * Isn't the name a bit ironic then?
+         */
+        inf->sig	&= 0xff;
+        return PTRACE__GROUPSTOP;
+      }
+
+  /* So we just have a 7 bit signal number.
+   *
+   * If it is SIGTRAP it might be a ptrace() signal.
+   * If it is some other, it might be a event or group stop.
+   * Else it is just a signal.
+   *
+   * This is all according to documentation which might
+   * have changed incompatible in the last -1 us, of course.
+   */
+  switch (inf->sig)
+    {
+    default:		/* signal by sure	*/
+      return PTRACE__SIGNAL;
+
+    case SIGSTOP:	/* might be group stop or signal	*/
+      if (inf->firststop)
+        {
+          inf->firststop = 0;
+          inf->sig	= 0;
+          return PTRACE__OPTIONS_MISSING;
+        }
+    case SIGTSTP:
+    case SIGTTIN:
+    case SIGTTOU:
+      if (!ptrace__getsiginfo(inf) || errno!=EINVAL)
+        return PTRACE__SIGNAL;
+      return PTRACE__GROUPSTOP;
+
+    case SIGTRAP:	/* might be ptrace() or signal	*/
+      if (ptrace__getsiginfo(inf))
+        return PTRACE__OPTIONS_MISSING;
+      break;
+    }
+
+  inf->sig	= 0;
+  if (inf->si.si_code != SIGTRAP &&
+      inf->si.si_code != (SIGTRAP | 0x80))
+    return PTRACE__TRAP;
+  return PTRACE__OPTIONS_MISSING;
+}
+
+static void
+ptrace__loop(struct trace_info *inf)
+{
   long	loops;
-  int	retval, retval_set;
-  char	*end;
-  int	detach;
+  pid_t	mainpid;
+  char	c;
 
-  /* Detaching is not so easy as one might think.
-   * As we are doing asynchronous things, we just cannot detach,
-   * as there may be a signal which then is spuriously delivered
-   * to the old parent if we detach.  So we must wait until we
-   * got the process stopped such that we can detach cleanly.
-   */
-  detach	= 0;
+  mainpid	= inf->pid;
 
-  /* If program is a PID and there are no args attach to the process.
-   */
-  if (isdigit(argv[0][0]) && !argv[1] && (pid=strtol(argv[0], &end, 0))!=0 && end && !*end)
-    {
-      ptrace__attach(pid);
-      ptrace__setoptions(pid);
+  inf->pid		= 0;
+  inf->retval		= 129;	/* default in case of termsig/exitsig	*/
+  inf->retval_set	= 0;
+  inf->firststop	= 1;
 
-#if 0
-      /* Just in case, revive process
-       */
-      ptrace__syscall(pid, 0);
-#endif
-
-      /* Try to detach cleanly in case we had a stopped process
-       */
-      detach	= !is_delay;
-    }
-  else if ((pid=fork())==0)
-    {
-      /* Fork a child and start tracing.
-       *
-       * I observed, that this creates a SIGTRAP, so this process is
-       * stopped for the wait below.
-       *
-       * If delay is set to 0 no trace is done.
-       */
-      if (is_delay)
-        ptrace__traceme();
-      /* Wait for the process
-       */
-      kill(getpid(), SIGSTOP);
-      /* Fork off the new process
-       */
-      execvp(argv[0], argv);
-      OOPS("fork failed: %s", argv[0]);
-    }
-  else if (pid==(pid_t)-1)
-    OOPS("fork");
-  else
-    status(pid, "started");
-
-  /* Start the wait loop
-   */
-  retval	= 129;	/* default in case of termsig/exitsig	*/
-  retval_set	= 0;
+  c			= 0;
   for (loops=1;;loops++)
     {
-      pid_t	pid2;
-
+      if (debug && c)
+        fputc(c, stderr);
+      c		= '*';
       errno	= 0;
-      pid2	= waitpid((pid_t)-1, &sta, WUNTRACED);
-      if (pid2==(pid_t)-1)
+      inf->pid	= waitpid((pid_t)-1, &inf->sta, WUNTRACED);
+      if (inf->pid == (pid_t)-1)
         {
           if (!errno || errno==EINTR || errno==EAGAIN)
             continue;
@@ -313,7 +383,7 @@ delay_trace(char **argv)
            */
           OOPS("waitpid");
         }
-      if (WIFEXITED(sta))
+      if (WIFEXITED(inf->sta))
         {
           /* Process terminated.
            *
@@ -321,51 +391,23 @@ delay_trace(char **argv)
            * childs might be traced (future!) we continue here.  The
            * wait above terminates anyways.
            */
-          status(pid2, "exit %d", WEXITSTATUS(sta));
-          if (pid2==pid)
+          status(inf->pid, "exit %d", WEXITSTATUS(inf->sta));
+          if (inf->pid == mainpid)
             {
-              retval		= WEXITSTATUS(sta);
-              retval_set++;
+              inf->retval	= WEXITSTATUS(inf->sta);
+              inf->retval_set++;
             }
+          continue;
         }
-      else if (WIFSIGNALED(sta))
+      if (WIFSIGNALED(inf->sta))
         {
           /* Process terminated due to signal.
            *
            * Usually you will see a warn "return value not set", as
            * the process is terminated without return value.
            */
-          status(pid2, "exit by signal %d", WTERMSIG(sta));
+          status(inf->pid, "exit by signal %d", WTERMSIG(inf->sta));
           /* WIFEXITED() from pid will follow	*/
-        }
-      else if (WIFSTOPPED(sta))
-        {
-          /* Process stopped due to ptrace() or signal
-           *
-           * If it's SIGTRAP it's ptrace() in our case, so do not
-           * report in this case.  There shall be some way to
-           * distinguish between ptrace() and "kill -TRAP", but I do
-           * not know any.
-           *
-           * For other signals you will see that it's delivered, which
-           * is not a bad "debugging sideeffect", I think ;)
-           */
-          if (!PTRACE__WIFPTRACED(sta))
-            status(pid2, "signal %d", WSTOPSIG(sta));
-
-          /* Do we want to detach cleanly?
-           */
-          else if (detach)
-            {
-              ptrace__detach(pid2);
-              if (pid == pid2)
-                break;
-            }
-
-          /* Now delay the processing
-           */
-          do_delay();
-          ptrace__syscall(pid2, (PTRACE__WIFPTRACED(sta) ? 0 : WSTOPSIG(sta)));
           continue;
         }
 #ifdef WCOREDUMP
@@ -373,32 +415,153 @@ delay_trace(char **argv)
        *
        * Implemented according to the manual.
        */
-      else if (WCOREDUMP(sta))
-        status(pid2, "coredump");
+      if (WCOREDUMP(inf->sta))
+        {
+          status(inf->pid, "coredump");
+          continue;
+        }
 #endif
-      /* Well, hit me, but sometimes things happen, which I want to
-       * know about.
+      if (!WIFSTOPPED(inf->sta))
+        {
+          /* Well, hit me, but sometimes things happen, which I want to
+           * know about.
+           */
+          warn("unknown wait status 0x%x", inf->sta);
+          continue;
+        }
+
+      /* Process stopped due to a signal.
+       *
+       * This can be SIGTRAP (from ptrace()) or not.
        */
-      else
-        warn("unknown wait status 0x%x", sta);
+
+      switch (ptrace__event(inf))
+        {
+        case PTRACE__GROUPSTOP:
+          status(inf->pid, "groupstop");
+          ptrace__listen(inf);	/* in this case we must listen	*/
+          continue;
+
+        case PTRACE__SIGNAL:	/* signal	*/
+          status(inf->pid, "signal %d", inf->sig);
+          break;
+
+        case PTRACE__OPTIONS_MISSING:
+          ptrace__setoptions(inf);
+        case PTRACE__SYSCALL:	/* syscall()	*/
+          /* Do we want to detach cleanly?
+           */
+          if (inf->detach)
+            {
+              ptrace__detach(inf);
+              if (inf->pid == mainpid)
+                return;
+            }
+          break;
+
+        case PTRACE__EVENTSTOP:
+          status(inf->pid, "Event %x", inf->evt);
+          break;
+
+        case PTRACE__TRAP:
+          /* I really have no idea why I get spurious TRAPs
+           * which must be suppresed to not make the child fail.
+           */
+          status(inf->pid, "TRAP ignored");
+          break;
+        }
+
+      /* Now delay the processing
+       */
+      do_delay(inf->delay);
+      if (inf->sig)
+        status(inf->pid, "deliver sig %d", inf->sig);
+      ptrace__syscall(inf);
+      c	= '#';
     }
   /* Warn if we came here without seeing a proper return value.
    */
-  if (!retval_set)
+}
+
+
+/* This routine is too long
+ */
+static int
+delay_trace(struct delay_info *delay, char **argv)
+{
+  struct trace_info	inf = { 0 };
+  char			*end;
+
+  inf.delay	= delay;
+  inf.options	= PTRACE_O_TRACESYSGOOD;
+
+  /* Detaching is not so easy as one might think.
+   * As we are doing asynchronous things, we just cannot detach,
+   * as there may be a signal which then is spuriously delivered
+   * to the old parent if we detach.  So we must wait until we
+   * got the process stopped such that we can detach cleanly.
+   */
+
+  /* If program is a PID and there are no args attach to the process.
+   */
+  if (isdigit(argv[0][0]) && !argv[1] && (inf.pid=strtol(argv[0], &end, 0))!=0 && end && !*end)
+    {
+      ptrace__attach(&inf);
+
+#if 0
+      /* Just in case, revive process
+       */
+      ptrace__syscall(pid, 0);
+#endif
+
+      /* Try to detach cleanly in case we had a stopped process
+       */
+      inf.detach	= !inf.delay->is_delay;
+    }
+  else if ((inf.pid=fork())==0)
+    {
+      /* Fork a child and start tracing.
+       *
+       * I observed, that this creates a SIGTRAP, so this process is
+       * stopped for the wait below.
+       *
+       * If delay is set to 0 no trace is done.
+       */
+      if (inf.delay->is_delay)
+        ptrace__traceme();
+      /* Wait for the process
+       */
+      raise(SIGSTOP);
+      /* Fork off the new process
+       */
+      execvp(argv[0], argv);
+      OOPS("fork failed: %s", argv[0]);
+    }
+  else if (inf.pid==(pid_t)-1)
+    OOPS("fork");
+  else
+    status(inf.pid, "started");
+
+  /* run it	*/
+  ptrace__loop(&inf);
+
+  if (!inf.retval_set)
     warn("return value not set");
+
   /* Report closing status
    */
-  status((pid_t)0, "%ld loops, %ld sleeps, retval %d", loops, sleeps, retval);
+  status((pid_t)0, "%lld loops, %lld sleeps, retval %d", inf.loops, inf.delay->sleeps, inf.retval);
+
   /* Terminate with the return value of the forked process
    *
    * Well, this is true in the attached case, so we are stealing the
    * return value in this case ;)
    */
-  return retval;
+  return inf.retval;
 }
 
 static int
-delay_copy(void)
+delay_copy(struct delay_info *delay)
 {
   int	got;
   char	buf[BUFSIZ];
@@ -415,7 +578,7 @@ delay_copy(void)
         {
           if (errno==EINTR || errno==EAGAIN)
             {
-              do_delay();
+              do_delay(delay);
               continue;
             }
           OOPS("stdin");
@@ -426,7 +589,7 @@ delay_copy(void)
        *
        * If is_delay is not set, output full blocks
        */
-      max	= is_delay ? 0 : got;
+      max	= delay->is_delay ? 0 : got;
       for (i=0; i<got; )
         {
           int	put;
@@ -437,7 +600,7 @@ delay_copy(void)
            *
            * I hate it to wait on the first round ;)
            */
-          do_delay();
+          do_delay(delay);
           if (put<0)
             {
               if (errno==EINTR || errno==EAGAIN)
@@ -449,7 +612,7 @@ delay_copy(void)
           i	+= put;
         }
     }
-  status((pid_t)0, "%ld loops, %ld sleeps, bytes %ld", loops, sleeps, bytes);
+  status((pid_t)0, "%ld loops, %lld sleeps, bytes %ld", loops, delay->sleeps, bytes);
   return 0;
 }
 
@@ -459,10 +622,11 @@ usage(void)
   fprintf(stderr,
           "Usage: %s [-options] delay [pid|program [args..]]\n"
           "\t\tVersion " SLOWDOWN_VERSION " compiled " __DATE__ "\n"
+          "\t-d\tdebug.  Print a # for each delay and * for other loops.\n"
 #if 0
-          "\t-s\tjail\n"	/* I forgot why I introduced this	*/
+          "\t-j\tjail.  Kill program if slowdown terminates\n"	/* PTRACE_O_EXITKILL */
           "\t-q\tquiet (suppress errors)\n"
-          "\t-r\trecursive (follow forks etc.)\n"
+          "\t-r\trecursive (follow forks etc.)\n"		/* PTRACE_O_TRACECLONE|PTRACE_O_TRACEFORK|PTRACE_O_TRACEVFORK */
 #endif
           "\t-v\tverbose status output\n"
           "\tdelay is in milliseconds.  delays below system counter\n"
@@ -493,6 +657,7 @@ setargs(int *argc, char ***argv)
   if ((tmp=strrchr(arg0, '/'))!=0)
     arg0	= tmp+1;
 
+  debug		= 0;
 #if 0
   jail		= 0;
   quiet		= 0;
@@ -505,6 +670,7 @@ setargs(int *argc, char ***argv)
       {
         switch (*tmp++)
           {
+          case 'd':	debug++; continue;
 #if 0
           case 'j':	jail++; continue;
           case 'q':	quiet++; continue;
@@ -524,22 +690,22 @@ setargs(int *argc, char ***argv)
 /* Calculate delay from given string argument
  */
 static void
-set_delay(const char *arg)
+set_delay(struct delay_info *delay, const char *arg)
 {
   char	*end;
 
-  delay_us	= strtoul(arg, &end, 0);
-  is_delay	= delay_us!=0;
+  delay->delay_us	= strtoul(arg, &end, 0);
+  delay->is_delay	= delay->delay_us!=0;
 
-  delay.tv_sec	= delay_us/1000;
-  delay.tv_nsec	= (delay_us%1000)*1000000ul;
+  delay->delay.tv_sec	= delay->delay_us/1000;
+  delay->delay.tv_nsec	= (delay->delay_us%1000)*1000000ul;
 
-  delay_us	*= 1000;
+  delay->delay_us	*= 1000;
 
   if (!end || *end)
     OOPS("please give a positive milliseconds for delay");
 
-  FATAL((delay.tv_sec*1000000ull+delay.tv_nsec/1000ul) != delay_us);
+  FATAL((delay->delay.tv_sec*1000000ull+delay->delay.tv_nsec/1000ul) != delay->delay_us);
 }
 
 /* slowdown [options] ms
@@ -551,11 +717,12 @@ set_delay(const char *arg)
 int
 main(int argc, char **argv)
 {
-  setargs(&argc, &argv);
+  static struct delay_info	delay = { 0 };
 
-  set_delay(argv[1]);
+  setargs(&argc, &argv);
+  set_delay(&delay, argv[1]);
   if (argc>2)
-    return delay_trace(argv+2);
-  return delay_copy();
+    return delay_trace(&delay, argv+2);
+  return delay_copy(&delay);
 }
 
